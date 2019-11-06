4894677f83672790b76ea6e7dbe0f0f6c5c55a25
thesachinchawla <thesachinchawla@gmail.com>
2019-11-06 18:22:49 +0530

    changes made into the design + added functionality there

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,5 +1,20 @@
 <?php 
     class Admin extends MY_Controller {
+
+        public function index(){
+            $this->form_validation->set_rules('username','Username','required');
+            $this->form_validation->set_rules('password','Password','required');
+
+            if($this->form_validation->run()){
+                echo "Validation Successful";
+            }
+            else {
+               $this->load->view('Users/ArticleList');
+
+            }
+
+
+        }
         
     }
 ?>
