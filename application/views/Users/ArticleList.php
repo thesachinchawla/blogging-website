9e8dab0f1dd9fc5d5fad6b0ecb8c767c5da879cf
thesachinchawla <thesachinchawla@gmail.com>
2019-11-06 13:20:09 +0530

    Adding content

--- application/views/Users/ArticleList.php
+++ application/views/Users/ArticleList.php
@@ -6,30 +6,35 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Article List</title>
     <link href="<?= base_url("assets/css/bootstrap.css"); ?>" rel="stylesheet" >
-    <script src="https://code.jquery.com/jquery-3.4.1.min.js"> </script>
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"> </script>  
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     
 
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Article List</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarColor02">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Login <span class="sr-only">(current)</span></a>
-    </li>   
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="text" placeholder="Search">
-      <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-    </form>
+  <!-- header --> 
+<?php include ('header.php'); ?>
+<div class = "container" style = "margin-top:20px" >
+<h1> Admin Form </h1>
+<?php echo form_open('Admin/index')?>
+  <div class="form-group">
+    <label for="Username">Username :</label>
+    <?php echo form_input(['class'=> 'form-control','placeholder'=>'Enter username']); ?>
   </div>
-</nav>
+  <div class="form-group">
+    <label for="pwd">Password:</label>
+    <?php echo form_password(['class'=>'form-control','type'=> 'password','placeholder'=>'Enter password']); ?>
+    </div>
+  <div class="form-group form-check">
+    <label class="form-check-label">
+      <input class="form-check-input" type="checkbox"> Remember me
+    </label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+
+
     
 </body>
 </html>
